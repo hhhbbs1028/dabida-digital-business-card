0f83f26ee4fb9e71126bbd335cdff742a0976c6e
hhhbbs1028 <hhhbbs1028@sookmyung.ac.kr>
2026-04-06 17:42:13 +0900

    fix: 공유 버튼 색상을 녹색에서 동일 명도의 파란색으로 변경

--- src/features/cards/components/CardsList.tsx
+++ src/features/cards/components/CardsList.tsx
@@ -192,7 +192,7 @@ export function CardsList({
                       <button
                         onClick={(e) => handleShare(e, card.id)}
                         className="flex w-20 flex-col items-center justify-center gap-1 text-white"
-                        style={{ background: '#5cb85c' }}
+                        style={{ background: '#5b8dd9' }}
                       >
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                           <path strokeLinecap="round" strokeLinejoin="round" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
